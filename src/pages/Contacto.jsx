f6ddfaa09aaec18d87a031f14c0c8ca2b60f509f
Factis1972 <foa1972@hotmail.com>
2025-12-02 17:04:49 -0300

    upd

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,6 +1,6 @@
 import Formulario from "../components/Formulario";
 import ImgContacto from "./../assets/ImgContacto.jpg";
-
+ 
 const Contacto = () => {
     return (
         <div className="flex flex-row static bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${ImgContacto})` } }  >
