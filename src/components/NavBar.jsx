ade4a5935cc4dc908456cf55c330263261db3d2b
Factis1972 <foa1972@hotmail.com>
2025-12-03 16:48:43 -0300

    upd

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,16 +10,26 @@ const NavBar = () => {
         <ul className="flex md:flex-row flex-col md:gap-10 gap-8 items-center md:items-center bg-transparent">
           <li className="flex mx-15 gap-10 flex-wrap wrap">
             <Link to="/" 
-            className="text-gray-700 md:text-lg text-2xl font-medium hover:text-black transition-colors duration-200 hover:underline underline-offset-4">Inicio</Link>
+            className="text-gray-700 md:text-lg text-2xl font-medium 
+                      hover:text-black hover:underline underline-offset-4 hover:decoration-green-700
+                      focus:underline decoration-2 decoration-gray-700">Inicio</Link>
             <Link to="/catalogo" 
-            className="text-gray-700 md:text-lg text-2xl font-medium hover:text-black transition-colors duration-200 hover:underline underline-offset-4">Pizzas Tradicionales</Link>
+            className="text-gray-700 md:text-lg text-2xl font-medium 
+                      hover:text-black hover:underline underline-offset-4 hover:decoration-green-700
+                      focus:underline decoration-2 decoration-gray-700">Pizzas Tradicionales</Link>
             <Link to="/especiales" 
-            className="text-gray-700 md:text-lg text-2xl font-medium hover:text-black transition-colors duration-200 hover:underline underline-offset-4">Pizzas Especiales</Link>
+            className="text-gray-700 md:text-lg text-2xl font-medium 
+                      hover:text-black hover:underline underline-offset-4 hover:decoration-green-700
+                      focus:underline decoration-2 decoration-gray-700">Pizzas Especiales</Link>
             <Link to="/contacto" 
-            className="text-gray-700 md:text-lg text-2xl font-medium hover:text-black transition-colors duration-200 hover:underline underline-offset-4">Contacto</Link>
+            className="text-gray-700 md:text-lg text-2xl font-medium 
+                      hover:text-black hover:underline underline-offset-4 hover:decoration-green-700
+                      focus:underline decoration-2 decoration-gray-700">Contacto</Link>
             {esAdmin && 
               <Link to="/Admin" 
-              className="text-gray-700 md:text-lg text-2xl font-medium hover:text-black transition-colors duration-200 hover:underline underline-offset-4">Admin</Link>
+              className="text-gray-700 md:text-lg text-2xl font-medium 
+                      hover:text-black hover:underline underline-offset-4 hover:decoration-green-700
+                      focus:underline decoration-2 decoration-gray-700">Admin</Link>
             }
           </li>
         </ul>
