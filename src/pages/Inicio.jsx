9ddbdfc98051ebda15ea32e4856a54bbc3379407
Factis1972 <foa1972@hotmail.com>
2025-12-04 08:45:12 -0300

    upd

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -4,7 +4,7 @@ const Inicio = () => {
   return(
     <>
       <div className="flex flex-row bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${Pizza})` } }  >
-        <div className="bg-white/50 backdrop-filter p-10 basis-2/5 text-2xl font-medium text-gray-900 md:text-overflow" >En nuestra pizzería italiana tradicional, cada receta cuenta una historia. <br /><br />
+        <div className="bg-white/50 backdrop-filter p-10 basis-2/5 text-2xl font-medium text-gray-900 md:text-overflow" >En nuestra tradicional pizzería italiana, cada receta cuenta una historia. <br /><br />
             Elaboramos nuestras pizzas con masa artesanal fermentada lentamente, ingredientes seleccionados y el inconfundible sabor que aporta el horno de piedra. <br /><br />
             Somos un espacio para compartir momentos, disfrutar de la calidez italiana y celebrar la pasión por la buena cocina.
         </div>
